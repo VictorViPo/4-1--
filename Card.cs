dec63b0e0dc7a484aec007ecc11d41d6a55c6a1e
VictorViPo <87740269+VictorViPo@users.noreply.github.com>
2022-08-30 20:13:59 +0300

    Add files via upload

---
+++ Card.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardStorageService.Data
+{
+    [Table("Cards")]
+    public class Card
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Cardid { get; set; }
+
+        [Column]
+        [StringLength(20)]
+        public string CardNo { get; set; }
+
+        [Column]
+        [StringLength(50)]
+        public string? Name { get; set; }
+
+        [Column]
+        [StringLength(50)]
+        public string? CW2 { get; set; }
+        
+        [Column]
+        public DateTime ExpDate { get; set; }
+
+    }
+}
---
+++ Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardStorageService.Data
+{
+    [Table("Clients")]
+    public class Client
+    {
+        public int Clientid { get; set; }
+
+        [Column]
+        [StringLength(255)]
+        public string? Surname { get; set; }
+
+        [Column]
+        [StringLength(255)]
+        public string? FirstName { get; set; }
+
+        [Column]
+        [StringLength(255)]
+        public string? Patronymic { get; set; }
+        
+    }
+}
