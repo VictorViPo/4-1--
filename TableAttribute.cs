d82a4f713d64d3ff46d2f972af8a794ca3c0dbc5
VictorViPo <87740269+VictorViPo@users.noreply.github.com>
2022-09-06 03:00:11 +0300

    Add files via upload

---
+++ TableAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CardStorageService
+{
+    internal class TableAttribute : Attribute
+    {
+        public TableAttribute(string v)
+        {
+            V = v;
+        }
+
+        public string V { get; }
+    }
+}
