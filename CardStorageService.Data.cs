4a3c0cde29ad0e660fba0a7651ab53d51caa6757
VictorViPo <87740269+VictorViPo@users.noreply.github.com>
2022-09-24 11:55:28 +0300

    Add files via upload

---
+++ CardStorageService.Data.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardStorageService.Data
+
+    public class CardStorageServiceDbContext : CardStorageServiceDbContext
+{
+    public virtual DbSet<Client> Clients { get; set; }
+    public virtual DbSet<Card> Cards { get; set; }
+
+    public CardStorageServiceDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConFiguring(DbContextionsBuilder optionsBuilder)
+    {
+        base.OnConFiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer();
+        optionsBuilder.UseLoadingProxies();
+    }
+    }
+}
